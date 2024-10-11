ee4463f8b38efc0fa524c54facaccdb5bd3c3654
Victor E González P <vemiliogp@gmail.com>
2024-10-11 19:18:01 +0200

    Initial commit

---
+++ products/src/bootstrap.js
@@ -0,0 +1,34 @@
+import faker from "faker";
+
+const mount = (el) => {
+  let products = "";
+
+  for (let i = 0; i < 5; i++) {
+    const name = faker.commerce.productName();
+    products += `<div>${name}</div>`;
+  }
+
+  el.innerHTML = products;
+};
+
+// Context/Situation #1
+// We are running this file in development or production
+// We are running standalone file
+// We are using an HTML file that has an element with an id of 'dev-products'
+// We want to immediately render our app into that element
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("#dev-products");
+
+  if (el) {
+    // Assuming our container doesn't have an element with an id of 'dev-products'
+    // We are probably running in isolation
+    mount(el);
+  }
+}
+
+// Context/Situation #2
+// We are running this file in development in isolation
+// We are using our local index.html file
+// Which DEFINITELY has an element with an id of 'dev-products'
+// We want to immediately render our app into that element
+export { mount };
